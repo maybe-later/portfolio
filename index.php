383429655bcc4935eaf3ab948dc897523811aa7e
maybe-later <42478812+maybe-later@users.noreply.github.com>
2018-08-18 14:38:01 +0100

    Prismic V2

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php include_once("home.html"); ?>
+<?php
+
+// In app/app.php, in place of the block starting with "$app->get('/',", put the following:
+$app->get('/', function ($request, $response, $args) use ($app, $prismic) {
+  // Query the API
+  $api = $prismic->get_api();
+  $document = $api->getByUID('page', 'quickstart');
+
+  // Render the page
+  render($app, 'home', array('document' => $document));
+});
+    ?>
