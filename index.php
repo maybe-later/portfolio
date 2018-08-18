4ac9dbe161d42af7c3f0e79bc7189ec5399f6707
maybe-later <42478812+maybe-later@users.noreply.github.com>
2018-08-18 14:54:10 +0100

    Prismic removed.

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php include_once("home.php"); ?>
-<?php
-
-// In app/app.php, in place of the block starting with "$app->get('/',", put the following:
-$app->get('/', function ($request, $response, $args) use ($app, $prismic) {
-  // Query the API
-  $api = $prismic->get_api();
-  $document = $api->getByUID('page', 'quickstart');
-
-  // Render the page
-  render($app, 'home', array('document' => $document));
-});
-    ?>
+<?php include_once("home.html"); ?>
